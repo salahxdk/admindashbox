5ce1b05bc848447688215b78dca789330e1f89ad
salahxdk <salahxdk22@gmail.com>
2023-08-24 21:51:33 -0400

    initial commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,30 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/salahxdk/admin-dashbrd/app/page.tsx": [
+      "/Users/salahxdk/admin-dashbrd/app/home.css"
+    ],
+    "/Users/salahxdk/admin-dashbrd/app/layout.tsx": [
+      "/Users/salahxdk/admin-dashbrd/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/salahxdk/admin-dashbrd/app/globals.css"
+    ],
+    "/Users/salahxdk/admin-dashbrd/app/dashboard/page.tsx": [
+      "/Users/salahxdk/admin-dashbrd/app/dashboard/prism.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/salahxdk/admin-dashbrd/app/page": [
+      "/Users/salahxdk/admin-dashbrd/app/home.css",
+      "/Users/salahxdk/admin-dashbrd/app/globals.css",
+      "/Users/salahxdk/admin-dashbrd/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/salahxdk/admin-dashbrd/app/sign-in/[[...sign-in]]/page": [
+      "/Users/salahxdk/admin-dashbrd/app/globals.css",
+      "/Users/salahxdk/admin-dashbrd/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/salahxdk/admin-dashbrd/app/dashboard/page": [
+      "/Users/salahxdk/admin-dashbrd/app/globals.css",
+      "/Users/salahxdk/admin-dashbrd/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/salahxdk/admin-dashbrd/app/dashboard/prism.css"
+    ]
+  }
+}
